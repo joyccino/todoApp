cdb2c5d3345ceaf61d1d5d3f8b0cbc6ed20a159f
Joyccino <67300266+joyccino@users.noreply.github.com>
2023-05-16 10:44:47 +0900

    springboot project

---
+++ todoproject/todoproject/src/test/java/com/atomic/todoproject/TodoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomic.todoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
